dae17350ade3847c3b75623a093a0c6c450e80d7
basel19-meet <basel19@meet.mit.edu>
2017-11-01 19:23:39 +0200

    cs is ok

---
+++ yl1201718/lab3/TurtleShapes.py
@@ -0,0 +1,17 @@
+import turtle
+
+#for i in range(5):
+#	turtle.forward(150)
+#	turtle.right(144)
+
+turtle.register_shape("DownArrow",((0,0),(-25,30),(-25,55),(25,55),(25,30),(0,0)))
+
+turtle.shape(DownArrow)
+
+ 
+
+
+
+
+
+turtle.mainloop()
